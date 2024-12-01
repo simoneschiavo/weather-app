b6718202722042861afb859b95d445e31f35c82e
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-01 10:17:36 +0100

    Add city form and styles

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const visualCrossingKey = "3U8A6THF3NYCX76N9T6G9PD8Y";
 const giphyKey = "PpZr2693AO0kM2kI84DeGQ2us3pNj3k8";
 const body = document.querySelector("body");
 
-const weatherInfoContainer = document.createElement("div");
-weatherInfoContainer.classList.add("weather-info")
+const weatherInfoContainer = document.querySelector(".weather-info");
 const cityName = document.createElement("p");
 cityName.classList.add("city");
 const dateAndTime = document.createElement("p");
@@ -18,8 +17,6 @@ const description = document.createElement("p");
 description.classList.add("description");
 
 weatherInfoContainer.append(cityName, dateAndTime, weatherImage, temperature, conditions, description);
-
-body.appendChild(weatherInfoContainer);
 
 async function getWeather(location) {
     const response = await fetch(
